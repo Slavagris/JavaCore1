bba7b3c0304d9a78ce4aa02c7e47aa0610359569
Slava Grishin <83882153+Slavagris@users.noreply.github.com>
2021-12-13 22:48:01 +0300

    Create Team.java

---
+++ Homework1/Team.java
@@ -0,0 +1,20 @@
+**
+ * JavaCore package Homework1
+ *
+ * @author Slava Grishin
+ * version 13.12.2021
+*/
+package Homework1;
+
+public class Team {
+    private Participant[] participants;
+
+    public Team(Participant... participants) {
+        // внутри метода переменное число параметров интерпретируется как массив
+        this.participants = participants;
+    }
+    public Participant[] getParticipants() {
+        return participants;
+    }
+
+}
