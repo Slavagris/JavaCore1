3f78b83245b3ecc50a683f00d912b07196e01665
Slava Grishin <83882153+Slavagris@users.noreply.github.com>
2022-01-19 22:59:58 +0300

    Update DailyForecast.java

--- Homework#7-8/DailyForecast.java
+++ Homework#7-8/DailyForecast.java
@@ -1,5 +1,11 @@
 package Homework7;
-
+/**
+* JavaСore 1 Homework#7-8
+*
+* @author Slava Grishin
+* version 17.01.2022
+* 
+*/
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
