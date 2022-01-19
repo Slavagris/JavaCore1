569a9f988f9f1cc38c09ff31e1a1a2dfb80d4349
Slava Grishin <83882153+Slavagris@users.noreply.github.com>
2022-01-19 23:03:22 +0300

    Update WeatherForecastView.java

--- Homework#7-8/WeatherForecastView.java
+++ Homework#7-8/WeatherForecastView.java
@@ -1,6 +1,12 @@
 package Homework7;
 
-
+/**
+* JavaСore 1 Homework#7-8
+*
+* @author Slava Grishin
+* version 17.01.2022
+* 
+*/
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
